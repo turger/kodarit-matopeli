7c264620f895a5ff551d67b1a4fe4d8de3473d94
Taru Airola <taru.airola@gmail.com>
2021-02-16 18:10:54 +0200

    Lisää muotoilut snake boardille

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "./SnakeBoard.css";
 
 const SnakeBoard = () => {
   const height = 10;
@@ -12,9 +13,13 @@ const SnakeBoard = () => {
   }
 
   const [rows, setRows] = useState(initialRows);
+  const [snake, setSnake] = useState([{x: 0, y: 0}]);
+  const [direction, setDirection] = useState("right");
 
   const displayRows = rows.map(row => (
-    <div className="Snake-row">{row.map(tile => <div className={tile} />)}</div>
+    <div className="Snake-row">
+      {row.map(tile => <div className={`tile ${tile}`} />)}
+    </div>
   ));
 
   return <div className="Snake-board">{displayRows}</div>;
