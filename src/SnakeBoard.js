3352e42c439758aeb0acd5fe7dce853aeb6b1763
Taru Airola <taru.airola@gmail.com>
2021-03-02 20:25:00 +0200

    Mato liikkuu oikealle

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {useInterval} from "./utils";
 import "./SnakeBoard.css";
 
 const SnakeBoard = () => {
@@ -21,6 +22,29 @@ const SnakeBoard = () => {
       {row.map(tile => <div className={`tile ${tile}`} />)}
     </div>
   ));
+
+  const displaySnake = () => {
+    const newRows = initialRows;
+    snake.forEach(tile => {
+      newRows[tile.x][tile.y] = "snake";
+    });
+    setRows(newRows);
+  };
+
+  const moveSnake = () => {
+    const newSnake = [];
+    switch (direction) {
+      case "right":
+        newSnake.push({x: snake[0].x, y: (snake[0].y + 1) % width});
+        break;
+      default:
+        break;
+    }
+    setSnake(newSnake);
+    displaySnake();
+  };
+
+  useInterval(moveSnake, 250);
 
   return <div className="Snake-board">{displayRows}</div>;
 };
